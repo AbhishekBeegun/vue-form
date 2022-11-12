d98e292c98fb05c3a20aad893ee765c25690bb9b
ABHISHEK BEEGUN <abhishekbeegun0642@gmail.com>
2022-11-11 23:34:30 -0800

    first-commit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,171 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Email </label>
+    <input type ="email" required v-model="email"/>
+
+    <label>Password</label>
+    <input type ="password" required v-model="password"/>
+    <div v-if="passwordError" class="Perror">{{passwordError}}</div>
+  
+    <label>Role:</label>
+    <select v-model="role">
+        <option value="Web Developer">Web Developer</option>
+        <option value="Web Designer">Web Designer</option>
+    </select>
+
+
+    <label>Skills : Press Alt to confirm Skill </label>
+    <input type="text" v-model="tempskills" @keyup="addSkills">
+    <div v-for="skill in skills" :key="skill" class="pill">
+        <span @click="Removeskill(skill)">{{skill}}</span>
+            
+    </div>
+
+    <div class="terms">
+        <input type="checkbox" v-model="terms" required>
+        <label>Accept Terms and Conditions</label>
+    </div>
+
+    <div class="submit">
+        <button>Create an account</button>        
+    </div>
+
+    
+  </form>
+
+  <p>Email:{{email}}</p>
+  <p>Password:{{password}}</p>
+  <p>Role: {{role}}</p>
+  <p>Skills : {{skills}}</p>
+  <p>Terms Accepted : {{terms}}</p>
+  
+
+ 
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email : '',
+            password:'',
+            role:'Web Developer',
+            terms:false,
+            tempskills:'',
+            skills:[],
+            passwordError:'',
+
+        }
+    },
+
+    methods:{
+        addSkills(e){
+            if (e.key === 'Alt' && this.tempskills){
+
+                if (!this.skills.includes(this.tempskills)){
+                    this.skills.push(this.tempskills)
+
+                }
+                
+                history.tempskills = ''
+            }
+        },
+
+
+        Removeskill(skill){
+            this.skills = this.skills.filter((item) => {
+                return skill !== item
+            })
+        },
+
+        handleSubmit(e){
+            //console.log('form submitted')
+
+            this.passwordError = this.password.length > 5 ?
+             '' : "Password Too short" 
+
+
+             if(!this.passwordError){
+                console.log('email:',this.email)
+                console.log('password:',this.password)
+                console.log('role:',this.role)
+                console.log('skills:',this.skills)
+                console.log('terms:',this.terms)
+             }
+        }
+
+    }
+
+
+}
+</script>
+
+<style>
+form{
+    width: 500px;
+    margin: 30px auto;
+    background: black;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+
+label{
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6rem;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+
+input, select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+}
+
+input[type="checkbox"]{
+    display: inline-block;
+    width:16px;
+    margin:0 10px 0 0;
+    position: relative;
+    top:2px;
+}
+
+.pill{
+    display: inline-block;
+    margin: 20px 10px 0 0 ;
+    padding: 6px 12px;
+    border-radius:20px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color:#777;
+    cursor: pointer;
+    text-transform: uppercase;
+}
+
+button{
+    background: cyan;
+    border: 0;
+    padding:10px 20px;
+    margin-top:20px;
+    color: black;
+    border-radius: 20px;
+}
+
+.submit{
+    text-align: center;
+}
+
+.Perror{
+    color:red;
+    margin-top: 10px;
+    font-size:0.8rem;
+    font-weight: bold ;
+}
+</style>
